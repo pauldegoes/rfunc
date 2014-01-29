c3cb098d6f6c4b6e5f99eb64d21fad96bfa13d09
Paul De Goes <paul.degoes@livingsocial.com>
2014-01-29 08:56:32 -0700

    add tests for module accessors

--- spec/rfunc_spec.rb
+++ spec/rfunc_spec.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
 
 describe RFunc do
-  it "allows for testing" do
-    RFunc::Seq.new([1,2,3]).map {|v| v * 2}.should eq([2, 4, 6])
+  include RFunc
+  describe "#seq" do
+    it "returns an RFunc::Seq" do
+      seq([1,2,3]).class.should == RFunc::Seq
+    end
+  end
+
+  describe "#option" do
+    it "returns the appropriate RFunc::Option" do
+      option(nil).class.should == RFunc::None
+      option("foo").class.should == RFunc::Some
+    end
+  end
+
+  describe "#some" do
+    it "returns the an RFunc::Some" do
+      some(1).should.class == RFunc::Some
+    end
+  end
+
+  describe "#none" do
+    it "returns the an RFunc::None" do
+      none.should.class == RFunc::None
+    end
   end
 end
